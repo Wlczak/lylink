b3549670600ce8dc911c7015920b80c3c5c6e4bd
Adam Vlček <vlcek-adam@seznam.cz>
2025-08-24 01:48:28 +0200

    simple setup

--- src/Router.php
+++ src/Router.php
@@ -1,2 +1,21 @@
 <?php
 declare (strict_types = 1);
+
+namespace Lylink;
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+class Router
+{
+    public static function handle(): void
+    {
+        SimpleRouter::get('/', [self::class, 'home']);
+
+        SimpleRouter::start();
+    }
+
+    public static function home(): string
+    {
+        return "Hello";
+    }
+}
