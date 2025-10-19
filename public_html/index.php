ad85623970f73ca8205e9f21ac1320e64d31a806
Adam Vlček <vlcek-adam@seznam.cz>
2025-10-19 14:51:44 +0200

    twig uses dev mode parameter

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
+use Dotenv\Dotenv;
 use Lylink\Router;
 
 session_start();
@@ -13,8 +14,13 @@ if (isset($_SESSION['email_verify'])) {
     }
 }
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../config');
+$dotenv->safeLoad();
+
+$devMode = $_ENV['DEV_MODE'] === "true" ? true : false;
 try {
     Router::handle();
+    Router::handle($devMode);
 } catch (Exception $e) {
     echo $e;
 }
--- src/Router.php
+++ src/Router.php
@@ -3,12 +3,10 @@ declare (strict_types = 1);
 
 namespace Lylink;
 
-use Dotenv\Dotenv;
 use Exception;
 use Lylink\Auth\AuthSession;
 use Lylink\Auth\DefaultAuth;
 use Lylink\Interfaces\Datatypes\PlaybackInfo;
-use Lylink\Interfaces\Datatypes\Track;
 use Lylink\Mail\Mailer;
 use Lylink\Models\Lyrics;
 use Lylink\Models\Settings;
@@ -23,14 +21,12 @@ use SpotifyWebAPI\SpotifyWebAPI;
 class Router
 {
     public static \Twig\Environment $twig;
-    public static function handle(): void
+    public static function handle(bool $devMode): void
     {
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../config');
-        $dotenv->safeLoad();
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
         self::$twig = new \Twig\Environment($loader, [
             'cache' => __DIR__ . '/../cache',
-            'debug' => true
+            'debug' => $devMode
         ]);
 
         ## User facing routes ##
